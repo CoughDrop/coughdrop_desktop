1c102856e07e3d6f7b78c0a93f3bf90ca9112939
Brian Whitmer <brian.whitmer@gmail.com>
2016-09-01 16:59:00 -0600

    update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,26 @@ module.exports = function (grunt) {
     grunt.initConfig({
         'create-windows-installer': {
             x64: {
-                appDirectory: './build/CoughDrop-win32-x64',
-                outputDirectory: './build/installer/64',
+                appDirectory: '../cdb/CoughDrop-win32-x64',
+                outputDirectory: '../cdb/inst/64',
                 authors: 'CoughDrop, Inc.',
-                exe: 'coughdrop.exe'
+                exe: 'coughdrop.exe',
+                title: 'CoughDrop',
+                iconUrl: 'https://www.mycoughdrop.com/app.ico',
+                loadingGif: 'loading.gif',
+                remoteReleases: 'https://s3.amazonaws.com/coughdrop/installer/windows/x64',
+                setupIcon: 'coughdrop.ico'
             },
             ia32: {
-                appDirectory: './build/CoughDrop-win32-ia32',
-                outputDirectory: './build/installer/32',
+                appDirectory: '../cdb/CoughDrop-win32-ia32',
+                outputDirectory: '../cdb/inst/32',
                 authors: 'CoughDrop, Inc.',
-                exe: 'coughdrop.exe'
+                exe: 'coughdrop.exe',
+                title: 'CoughDrop',
+                iconUrl: 'https://www.mycoughdrop.com/app.ico',
+                loadingGif: 'loading.gif',
+                remoteReleases: 'https://s3.amazonaws.com/coughdrop/installer/windows/ia32',
+                setupIcon: 'coughdrop.ico'
             }
         }
     });
