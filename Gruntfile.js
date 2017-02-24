79c64e232c7bd324f564566b169c0caf4b59e272
Brian Whitmer <brian.whitmer@gmail.com>
2017-02-24 14:42:06 -0700

    fix packaging process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,40 @@
 module.exports = function (grunt) {
+    var signing = null;
+    try {
+        signing = grunt.file.readJSON('../cdcert.json');
+    } catch (e) { }
+    var opts64 = {
+        appDirectory: '../cdb/CoughDrop-win32-x64',
+        outputDirectory: '../cdb/inst/64',
+        authors: 'CoughDrop, Inc.',
+        exe: 'coughdrop.exe',
+        title: 'CoughDrop',
+        iconUrl: 'https://www.mycoughdrop.com/app.ico',
+        loadingGif: 'loading.gif',
+        remoteReleases: 'https://s3.amazonaws.com/coughdrop/installer/windows/x64',
+        setupIcon: 'coughdrop.ico'
+    };
+    var opts32 = {
+        appDirectory: '../cdb/CoughDrop-win32-ia32',
+        outputDirectory: '../cdb/inst/32',
+        authors: 'CoughDrop, Inc.',
+        exe: 'coughdrop.exe',
+        title: 'CoughDrop',
+        iconUrl: 'https://www.mycoughdrop.com/app.ico',
+        loadingGif: 'loading.gif',
+        remoteReleases: 'https://s3.amazonaws.com/coughdrop/installer/windows/ia32',
+        setupIcon: 'coughdrop.ico'
+    };
+    if (signing && signing.password && signing.cert) {
+        opts64.certificateFile = signing.cert;
+        opts64.certificatePassword = signing.password;
+        opts32.certificateFile = signing.cert;
+        opts32.certificatePassword = signing.password;
+    }
     grunt.initConfig({
         'create-windows-installer': {
-            x64: {
-                appDirectory: '../cdb/CoughDrop-win32-x64',
-                outputDirectory: '../cdb/inst/64',
-                authors: 'CoughDrop, Inc.',
-                exe: 'coughdrop.exe',
-                title: 'CoughDrop',
-                iconUrl: 'https://www.mycoughdrop.com/app.ico',
-                loadingGif: 'loading.gif',
-                remoteReleases: 'https://s3.amazonaws.com/coughdrop/installer/windows/x64',
-                setupIcon: 'coughdrop.ico'
-            },
-            ia32: {
-                appDirectory: '../cdb/CoughDrop-win32-ia32',
-                outputDirectory: '../cdb/inst/32',
-                authors: 'CoughDrop, Inc.',
-                exe: 'coughdrop.exe',
-                title: 'CoughDrop',
-                iconUrl: 'https://www.mycoughdrop.com/app.ico',
-                loadingGif: 'loading.gif',
-                remoteReleases: 'https://s3.amazonaws.com/coughdrop/installer/windows/ia32',
-                setupIcon: 'coughdrop.ico'
-            }
+            x64: opts64,
+            ia32: opts32
         }
     });
 
--- packaging_prep.js
+++ packaging_prep.js
@@ -1,6 +1,11 @@
 var cp = require('child_process');
 var packager = require('electron-packager');
+var file = require('fs');
 
+// read package.json, get
+var json = JSON.parse(file.readFileSync("package.json", { encoding: 'utf-8' }));
+var version = json.version;
+console.log("version", version);
 // "package": "node packaging_prep.js x64 && electron-packager ./ CoughDrop --overwrite --prune --asar=true --platform=win32 --arch=all --icon=\"coughdrop.ico\" --ignore=\"build\" --ignore=\"data\" --out=\"../cdb\" --version=1.3.4 --version-string.CompanyName=\"CoughDrop\" --version-string.ProductName=\"CoughDrop\"",
 packager({
   dir: "./",
@@ -14,38 +19,43 @@ packager({
   ignore: ["build", "data", "files"],
   out: "../cdb",
   version: "1.3.4",
+  appVersion: version,
   "version-string": {
     FileDescription: "CoughDrop",
+    FileVersion: version,
     CompanyName: "CoughDrop",
     ProductName: "CoughDrop",
+    ProductVersion: version,
     OriginalFilename: "coughdrop.exe"
   },
   win32metadata: {
     FileDescription: "CoughDrop",
+    FileVersion: version,
     CompanyName: "CoughDrop",
     ProductName: "CoughDrop",
+    ProductVersion: version,
     OriginalFilename: "coughdrop.exe"
   },
   afterCopy: [function (path, version, platform, arch, done) {
       path = path.replace(/\\resources\\app/, '');
       console.log("installed temporarily to " + path);
       var cmd = "md " + path + "\\resources\\app\\node_modules\\form-data";
-      cmd = cmd + " & xcopy .\\node_modules\\form-data\\* " + path + "\\resources\\app\\node_modules\\form-data /Y /S";
-      cmd = cmd + " && copy .\\AcaTTS.* " + path + " /Y";
-      cmd = cmd + " && copy .\\coughdrop.ico " + path + " /Y";
-      cmd = cmd + " && copy .\\logo.png " + path + " /Y";
+      cmd = cmd + "  & xcopy .\\node_modules\\form-data\\* " + path + "\\resources\\app\\node_modules\\form-data /Y /S";
+      cmd = cmd + " && echo a && xcopy .\\bin " + path + "\\bin /Y /E /I";
+      cmd = cmd + " && echo b && copy .\\AcaTTS.* " + path + " /Y";
+      cmd = cmd + " && echo c && copy .\\coughdrop.ico " + path + " /Y";
+      cmd = cmd + " && echo d && copy .\\logo.png " + path + " /Y";
+//      cmd = cmd + " && echo d && rmdir " + path + "\\resources\\app\\files /s /q";
       if (arch == 'x64') {
-          cmd = cmd + " && copy ..\\eyex\\x64\\Tobii.EyeX.*.dll " + path + " /Y";
-          cmd = cmd + " && copy ..\\eyex\\eyex.64.node " + path + "\\resources\\app\\node_modules\\eyex\\eyex.node /Y";
-          cmd = cmd + " && copy ..\\acapela\\acapela.64.node " + path + "\\resources\\app\\node_modules\\acapela\\acapela.node /Y";
+          cmd = cmd + " && echo e && copy ..\\eyex\\lib\\x64\\Tobii.EyeX.*.dll " + path + " /Y";
+          cmd = cmd + " && echo f && copy ..\\eyex\\eyex.64.node " + path + "\\resources\\app\\node_modules\\eyex\\eyex.node /Y";
+          cmd = cmd + " && echo g && copy ..\\acapela\\acapela.64.node " + path + "\\resources\\app\\node_modules\\acapela\\acapela.node /Y";
       } else {
-          cmd = cmd + " && copy ..\\eyex\\x86\\Tobii.EyeX.*.dll " + path + " /Y";
-          cmd = cmd + " && copy ..\\eyex\\eyex.32.node " + path + "\\resources\\app\\node_modules\\eyex\\eyex.node /Y";
-          cmd = cmd + " && copy ..\\acapela\\acapela.32.node " + path + "\\resources\\app\\node_modules\\acapela\\acapela.node /Y";
+          cmd = cmd + " && echo h && copy ..\\eyex\\lib\\x86\\Tobii.EyeX.*.dll " + path + " /Y";
+          cmd = cmd + " && echo j && copy ..\\acapela\\acapela.32.node " + path + "\\resources\\app\\node_modules\\acapela\\acapela.node /Y";
+          cmd = cmd + " && echo i && copy ..\\eyex\\eyex.32.node " + path + "\\resources\\app\\node_modules\\eyex\\eyex.node /Y";
       }
-      cmd = cmd + " && md " + path + "\\bin";
-      cmd = cmd + " & copy .\\bin\\* " + path + "\\bin /Y";
-      cmd = cmd + " && rmdir " + path + "\\resources\\app\\files /s /q";
+      cmd = cmd + " && echo \"done with actions\"";
       var child = cp.exec(cmd);
       console.log("running for " + arch);
       console.log(cmd);
