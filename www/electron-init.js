5cf3ec690e1ce6d55a9f93612a680dad4858c64b
Brian Whitmer <brian.whitmer@gmail.com>
2017-07-25 13:26:32 -0600

    client-size node requires, cleaned up

--- www/electron-init.js
+++ www/electron-init.js
@@ -1,5 +1,7 @@
 // Fix for Electron and Ember using different require methods,
 // see https://github.com/atom/electron/issues/3423
 window.nodeModule = window.module;
-window.requireNode = window.nodeModule.require;
+window.requireNode = function (lookup) {
+  return window.nodeModule.require(lookup);
+}
 window.module = undefined;
--- www/ipc.js
+++ www/ipc.js
@@ -1,30 +1,31 @@
 var ipcRenderer = requireNode('electron').ipcRenderer;
-var path = '../node_modules/gazelinger/electron-listener.js';
-var tts_path = '../node_modules/acapela/extra-tts-ipc.js';
-if(location.pathname) {
-  path = location.pathname.replace(/^\//, '').replace(/www\/desktop_index\.html.*$/, 'node_modules/gazelinger/electron-listener.js');
-  tts_path = location.pathname.replace(/^\//, '').replace(/www\/desktop_index\.html.*$/, 'node_modules/acapela/extra-tts-ipc.js');
-}
-var eye_gaze = requireNode(path);
-if(window.capabilities) {
-  window.capabilities.eye_gaze = window.capabilities.eye_gaze || {};
-  window.capabilities.eye_gaze.listen = eye_gaze.listen;
-  window.capabilities.eye_gaze.stop_listening = eye_gaze.stop_listening;
-  window.capabilities.eye_gaze.available = true;
-}
 
-var tts = requireNode(tts_path);
-if (window.capabilities) {
-    tts.reload = tts.reload || function () { };
-    window.extra_tts = tts;
-    window.capabilities.tts.extra_exec = tts.exec;
-    window.capabilities.debugging = {
-      available: function() { return true; },
-      show: function() {
-        ipcRenderer.send('debugging-show', 'show');
-      }
-    };
-}
+try {
+    var path = 'gazelinger/electron-listener.js';
+    var eye_gaze = requireNode(path);
+    if (window.capabilities) {
+        window.capabilities.eye_gaze.listen = eye_gaze.listen;
+        window.capabilities.eye_gaze.stop_listening = eye_gaze.stop_listening;
+        window.capabilities.eye_gaze.available = true;
+    }
+} catch (e) { }
+
+try {
+    var tts_path = 'acapela/extra-tts-ipc.js';
+    var tts = requireNode(tts_path);
+    if (window.capabilities) {
+        tts.reload = tts.reload || function () { };
+        window.extra_tts = tts;
+        window.capabilities.tts.extra_exec = tts.exec;
+        window.capabilities.debugging = {
+            available: function () { return true; },
+            show: function () {
+                ipcRenderer.send('debugging-show', 'show');
+            }
+        };
+    }
+} catch (e) { }
+
 var ds = 0;
 document.addEventListener('keypress', function (e) {
     if (e.target.tagName != 'INPUT' && e.target.tagName != 'TEXTAREA' && (e.keyCode == 100 || e.keyCode == 68) && !document.getElementById('home_button')) {
