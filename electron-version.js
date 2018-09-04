fae5550f3133c67a677f4fa34e36783bfeb80f9a
Brian Whitmer <brian.whitmer@gmail.com>
2018-09-04 10:53:40 -0600

    use electron version from package.json

--- electron-version.js
+++ electron-version.js
@@ -1,4 +1,13 @@
+// parse package.json
+// use the version specified for electron in package.json
+var fs = require('fs');
+var str = fs.readFileSync('./package.json')
+var json = JSON.parse(str);
+var version = "0.0.0";
+if(json && json['dependencies'] && json['dependencies']['electron']) {
+    version = json['dependencies']['electron'];
+}
 module.exports = {
-    version: "1.8.2"
+    version: version
 };
-console.log(module.exports.version)
+console.log(module.exports.version);
