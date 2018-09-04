f09a2bdabf6fc939685248729b251958bc5c17f6
Brian Whitmer <brian.whitmer@gmail.com>
2018-09-04 11:32:20 -0600

    shorten timeouts, prolly not needed at all

--- rebuild-acapela.cmd
+++ rebuild-acapela.cmd
@@ -2,4 +2,4 @@ for /f "delims=" %%b in ('node electron-version.js') do @set arg_target=%%b
 set arg_arch=%1
 set arg_archnum=%2
 echo REBUILDING FOR v%
-cd ..\\acapela2 && node-gyp clean && timeout 5 && node-gyp configure --target=v%arg_target% --arch=%arg_arch% && timeout 5 && node ..\\coughdrop_desktop\\fix_node.js && timeout 5 && node-gyp build && copy build\\Release\\acapela.node acapela.%arg_archnum%.node /Y
+cd ..\\acapela && node-gyp clean && timeout 2 && node-gyp configure --target=v%arg_target% --arch=%arg_arch% && timeout 2 && node ..\\coughdrop_desktop\\fix_node.js && timeout 2 && node-gyp build && copy build\\Release\\acapela.node acapela.%arg_archnum%.node /Y
